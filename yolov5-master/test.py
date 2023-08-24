72af124738065606d0a827cc608192abb17a3eeb
forfourr <iiilee0404@gmail.com>
2023-08-24 20:03:47 +0900

    yolov5 test 코드 수정

--- yolov5-master/test.py
+++ yolov5-master/test.py
@@ -4,7 +4,7 @@ import glob
 import cv2
 import edgetpu.detection.engine
 
-model_path = "yolov5-master\runs\train\exp2\weights\best.pb"
+model_path = "yolov5-master/runs/train/exp2/weights/best-fp16.tflite"
 engine = edgetpu.detection.engine.DetectionEngine(model_path)
 # model = torch.hub.load("ultralytics/yolov5", 'custom', path="./runs/train/exp2/weights/best.pt")
 # model.cuda()
@@ -12,7 +12,7 @@ engine = edgetpu.detection.engine.DetectionEngine(model_path)
 # print(model)D
 
 # model cofiger
-# model.conf = 0.65
+# model.score = 0.65
 # model.iou = 0.45
 
 label_dict = {
@@ -24,27 +24,26 @@ label_dict = {
     5: "human"
 }
 
-img_folder_path = "./dataset_final/test"
-img_path = glob.glob(os.path.join(img_folder_path, "*", "*.jpg"))
+img_path = 'test.png'
 
-for path in img_path:
-    image = cv2.imread(path)
-    #result = model(path, size=640)
-    results = engine.detect_with_input_tensor(image, threshold=0.65, keep_aspect_ratio=True, relative_coord=False)
+image = cv2.imread(img_path)
+#result = model(path, size=640)
+results = engine.detect_with_input_tensor(image, threshold=0.65, keep_aspect_ratio=True, relative_coord=False)
 
-    #bboxes = result.xyxy[0]
-    for obj in results:
-        bbox = obj.bounding_box.flatten().tolist()
-        class_id = obj.label
-        conf = obj.score
+#bboxes = result.xyxy[0]
+for obj in results:
+    bbox = obj.bounding_box.flatten().tolist()
+    label = obj.label
+    score = obj.score
+    print(f"Label: {label}, Score: {score:.2f}, Box: {bbox}")
 
-        x1, y1, x2, y2 = bbox
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        cls_name = label_dict[class_id]
-        image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    x1, y1, x2, y2 = bbox
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    cls_name = label_dict[label]
+    image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        print(x1, y1, x2, y2, conf, cls_name)
+    print(x1, y1, x2, y2, score, cls_name)
 
-    cv2.imshow("test", image)
-    if cv2.waitKey(0) & 0xFF == ord('q') :
-        exit()
+cv2.imshow("test", image)
+if cv2.waitKey(0) & 0xFF == ord('q') :
+    exit()
