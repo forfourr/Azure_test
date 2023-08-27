3a774f48a3d264683c3095eeada9734ff440f0e5
forfourr <iiilee0404@gmail.com>
2023-08-27 15:23:47 +0900

    test

--- yolov5-master/test copy.py
+++ yolov5-master/test copy.py
@@ -1,8 +1,20 @@
-import os
-#import torch
-import glob
+import argparse
+import time
+from pathlib import Path
+
 import cv2
-import edgetpu.detection.engine
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+import numpy as np
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+    strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+import importlib.util
+from tflite_runtime.interpreter import load_delegate
+
 
 model_path = "runs/train/exp2/weights/best-fp16.tflite"
 engine = edgetpu.detection.engine.DetectionEngine(model_path)
